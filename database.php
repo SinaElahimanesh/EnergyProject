8df8b4974ccdaba2cf3b765f9adf2861eee6206b
Alireza <alirezaeiji151379@gmail.com>
2021-01-25 23:12:42 +0330

    کلاس دیتا بیس شروع شد!

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class database
+{
+    private $connection;
+
+    function __construct()
+    {
+       $this->connection = new mysqli("localHost","alirezaeiji151379","alirezaeiji","energy_Project");
+    }
+
+    function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+        $this->connection->close();
+    }
+
+    public function query($queryStr){
+        if(is_null($this->connection)){
+            return;
+        }
+        else{
+            return $this->connection->query($queryStr);
+        }
+    }
+
+
+}
